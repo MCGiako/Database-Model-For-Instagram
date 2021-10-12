c120faf41dc18134d616e95250a5da1d998bbbfd
MCGiako <giacometto.r.h2@gmail.com>
2021-10-12 23:43:51 +0000

    Code Terminado

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,24 +8,34 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Usuario(Base):
+    __tablename__ = 'usuario'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    nombredeusuario = Column(String(250), nullable=False)
+    nombre = Column(String(250), nullable=False)
+    apellidos = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = 'follower'
+    usuario_from_id = Column(String(250), ForeignKey('usuario.id'),primary_key=True)
+    usuario_to_id = Column(String(250), ForeignKey('usuario.id'))
+
+class Comentario(Base):
+    __tablename__ = 'comentario'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    comentario_texto = Column(String(250))
+    autor_id = Column(Integer,ForeignKey('usuario.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
 
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    usuario_to_id = Column(String(250))    
+
+   
     def to_dict(self):
         return {}
 
